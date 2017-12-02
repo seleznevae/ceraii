f6c19bb80d90cc92bdff3c01916ee3d4179c8a5c
seleznevae <seleznevae@protonmail.com>
2017-12-02 20:15:13 +0300

    [A] Added special test build

--- tests/multi_threads.c
+++ tests/multi_threads.c
@@ -49,6 +49,9 @@ void* thread_1_func(void *arg)
         marker3[thread_nuber] = 0;
         int result = base_multiple_return_100(thread_nuber);
         assert(result == 100);
+        assert(marker1[thread_nuber] = 1);
+        assert(marker3[thread_nuber] = 2);
+        assert(marker2[thread_nuber] = 3);
     }
     return NULL;
 }
@@ -63,6 +66,9 @@ void* thread_2_func(void *arg)
         marker3[thread_nuber] = 0;
         int result = base_multiple_return_100(thread_nuber);
         assert(result == 100);
+        assert(marker1[thread_nuber] = 1);
+        assert(marker3[thread_nuber] = 2);
+        assert(marker2[thread_nuber] = 3);
     }
     return NULL;
 }
@@ -77,17 +83,38 @@ void* thread_3_func(void *arg)
         marker3[thread_nuber] = 0;
         int result = base_multiple_return_100(thread_nuber);
         assert(result == 100);
+        assert(marker1[thread_nuber] = 1);
+        assert(marker3[thread_nuber] = 2);
+        assert(marker2[thread_nuber] = 3);
     }
     return NULL;
 }
 
 
+void* thread_func(void *arg)
+{
+    int thread_number = *(int*)arg;
+    for (size_t i = 0; i < 100/*NUMBER_OF_ITERATIONS*/; ++i) {
+        marker1[thread_number] = 0;
+        marker2[thread_number] = 0;
+        marker3[thread_number] = 0;
+        int result = base_multiple_return_100(thread_number);
+        assert(result == 100);
+        assert(marker1[thread_number] = 1);
+        assert(marker3[thread_number] = 2);
+        assert(marker2[thread_number] = 3);
+    }
+    return NULL;
+}
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
 
+    /*
+     * Test threads that run independent functions with CERAII macros
+     */
     pthread_t thread_1;
     pthread_t thread_2;
     pthread_t thread_3;
@@ -117,5 +144,39 @@ int main(int argc, char *argv[])
     pthread_join(thread_2, NULL);
     pthread_join(thread_1, NULL);
 
+
+    /*
+     * Test threads that run the same functions with CERAII macros
+     */
+    int thread1_number = 0;
+    status = pthread_create(&thread_1, NULL, &thread_func, &thread1_number);
+    if (status != 0) {
+        fprintf(stderr, "pthread_create failed\n");
+        return 1;
+    }
+
+    int thread2_number = 1;
+    status = pthread_create(&thread_2, NULL, &thread_func, &thread2_number);
+    if (status != 0) {
+        fprintf(stderr, "pthread_create failed\n");
+        pthread_join(thread_1, NULL);
+        return 1;
+    }
+
+    int thread3_number = 2;
+    status = pthread_create(&thread_3, NULL, &thread_func, &thread3_number);
+    if (status != 0) {
+        fprintf(stderr, "pthread_create failed\n");
+        pthread_join(thread_2, NULL);
+        pthread_join(thread_1, NULL);
+        return 1;
+    }
+
+    pthread_join(thread_3, NULL);
+    pthread_join(thread_2, NULL);
+    pthread_join(thread_1, NULL);
+
+    int* arg = (int*)malloc(10);
+    *arg = 10;
     return 0;
 }
